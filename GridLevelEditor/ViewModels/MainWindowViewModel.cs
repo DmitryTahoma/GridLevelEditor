b43d675bc7d69dc27894de44a2ee3682ff6ab1b9
DmitryTahoma <dimon.otrishk@gmail.com>
2021-01-24 11:10:22 +0200

    init commit

---
+++ GridLevelEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Catel.Data;
+using Catel.MVVM;
+
+namespace GridLevelEditor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            TitleText = "Grid Level Editor v1.0";
+        }
+
+        public string TitleText
+        {
+            get => GetValue<string>(TitleTextProperty);
+            set => SetValue(TitleTextProperty, value);
+        }
+        public static readonly PropertyData TitleTextProperty = RegisterProperty(nameof(TitleText), typeof(string), "");
+    }
+}
